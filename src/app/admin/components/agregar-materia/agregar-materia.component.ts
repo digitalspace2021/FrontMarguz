1dbf6c335774d14e81c114bf1263c02d5bbc619d
jhonatan1043 <jhonat.rodri@gmail.com>
2022-01-09 18:49:59 -0500

    terminando cambios en materia

--- src/app/admin/components/agregar-materia/agregar-materia.component.ts
+++ src/app/admin/components/agregar-materia/agregar-materia.component.ts
@@ -1,5 +1,5 @@
-import { IMateria } from './../../interfaces/IMateria';
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ListaMateriasComponent } from './../../containers/lista-materias/lista-materias.component';
+import { Component, Input, OnInit } from '@angular/core';
 import { Materia } from '../../class/Materia';
 import { MateriaService } from '../../services/materia.service';
 import Swal from 'sweetalert2';
@@ -15,12 +15,14 @@ export class AgregarMateriaComponent implements OnInit {
   @Input() materia: Materia = new Materia();
   @Input() action: boolean = false;
   @ViewChild('ModalClose') ModalClose?: ElementRef;
-  @Output() response: EventEmitter<IMateria> = new EventEmitter();
 
   materiaInput: string = '';
   val: boolean = false;
 
-  constructor(private materiaSv: MateriaService) {}
+  constructor(
+    private materiaSv: MateriaService,
+    private materiaL: ListaMateriasComponent
+  ) {}
 
   ngOnInit(): void {}
 
@@ -41,8 +43,8 @@ export class AgregarMateriaComponent implements OnInit {
         materia.materia = this.materiaInput;
         this.materiaSv.createMateria(materia).subscribe((resp) => {
           if (resp) {
-            this.response.emit(resp);
             Swal.fire('Registro exitoso...', 'Guardado', 'success').then(() => {
+              this.materiaL.listMateria();
               this.ModalClose?.nativeElement.click();
             });
           }
@@ -52,9 +54,9 @@ export class AgregarMateriaComponent implements OnInit {
         materia.materia = this.materiaInput;
         this.materiaSv.updateMateria(materia).subscribe((resp) => {
           if (resp) {
-            this.response.emit(resp);
             Swal.fire('Registro exitoso...', 'Actualizado', 'success').then(
               () => {
+                this.materiaL.listMateria();
                 this.ModalClose?.nativeElement.click();
               }
             );
