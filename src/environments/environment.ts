35f01192c470fdae242d88ba43179f715eecccfc
Nestor710 <nestorlima710@gmail.com>
2022-04-05 16:22:06 -0500

    Se arreglo bug en el formulario al crear un usuario admin, profesor o estudiante desde el admin

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  host: 'http://marguzbackendvfinal.test/api/v1/',
+  host: 'http://127.0.0.1:8000/api/v1/',
+  //host: 'http://marguzbackendvfinal.test/api/v1/',
   // host: 'https://api.marguz.co/api/v1/',
   //host: 'https://api.marguz.co/api/v1/',
 };
